191260e3df5dd633d56fac4c12bd7e08ad658b8d
salamandermike <salamandermike@gmail.com>
2014-09-09 11:49:18 -0700

    notes merge 9.9.14

--- week_07_rails/day_03_nested_resources_and_forms/dusk_resources_and_nested_resources/bog_app/app/models/creature.rb
+++
@@ -1,3 +0,0 @@
-class Creature < ActiveRecord::Base
-  has_many :comments
-end
--- week_07_rails/day_03_nested_resources_and_forms/dusk_resources_and_nested_resources/bog_app/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  root to: "creatures#index"
-  resources :creatures do
-    resources :comments
-  end
-end
